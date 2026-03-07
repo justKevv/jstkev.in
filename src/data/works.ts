0d13758d8c544c9fc873de6354e8d98620bf4c99
justKevv <kevinbramastaa@gmail.com>
2026-03-07 23:22:47 +0800

    feat(works): add workscard component and associated data for showcasing work projects

---
+++ src/data/works.ts
@@ -0,0 +1,16 @@
+export const works = [
+  {
+    company: "independent",
+    role: "Full Stack Developer",
+    stack: ['php', 'laravel', 'filament', 'livewire', 'tailwind', 'mysql'],
+    type: "freelance",
+    date: "dec 2025 - feb 2026",
+    location: "indonesia",
+    highlights: [
+      "tasked to continue an existing laravel app — a nursing workload calculator for hospitals",
+      "started clean but hit a wall when requirements grew to support multiple hospitals",
+      "recognized it was becoming tech debt, so i reworked the database from scratch",
+      "migrated the admin panel from bootstrap to filament for a more maintainable and efficient setup",
+    ]
+  },
+]
