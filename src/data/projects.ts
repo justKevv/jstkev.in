9748370565b10d8707cdd4bfbc6f2b0dc0149c61
justKevv <kevinbramastaa@gmail.com>
2026-03-08 14:29:37 +0800

    fix(ui): update project description for jsta.dev to reflect continuous improvement

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -4,7 +4,7 @@ import { SITE_URL } from "../config";
 export const projects: ProjectItem[] = [
   {
     name: "jsta.dev",
-    description: "my personal website, built with astro. it's a work in progress, but it's a great way to showcase my skills and projects.",
+    description: "my personal website, built with astro. it's always work in progress, but it's a great way to showcase my skills and projects.",
     stack: ['astro', 'gsap', 'typescript', 'tailwindcss'],
     date: "2026",
     github: "https://github.com/justKevv",
