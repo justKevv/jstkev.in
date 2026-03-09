ea1b24bdfbf7e2f4cde8940c1096235548596b9a
justKevv <kevinbramastaa@gmail.com>
2026-03-10 00:27:43 +0800

    feat(database): add stacksurls object for storing technology stack urls

---
+++ src/data/stacksUrls.ts
@@ -0,0 +1,7 @@
+export const stackUrls: Record<string, string> = {
+  astro: "https://astro.build",
+  typescript: "https://www.typescriptlang.org/",
+  gsap: "https://gsap.com/",
+  tailwindcss: "https://tailwindcss.com",
+  // add more
+}
