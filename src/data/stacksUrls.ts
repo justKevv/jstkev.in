1696a29af67223d90b775751086e334bf19a1b26
justKevv <kevinbramastaa@gmail.com>
2026-03-11 23:11:48 +0800

    feat(data): add tech stack urls for documentation and reference

--- src/data/stacksUrls.ts
+++ src/data/stacksUrls.ts
@@ -5,5 +5,11 @@ export const stackUrls: Record<string, string> = {
   tailwindcss: "https://tailwindcss.com",
   nextjs: "https://nextjs.org/",
   sanityio: "https://www.sanity.io/",
+  python: "https://www.python.org",
+  arduino: "https://www.arduino.cc",
+  flask: "https://flask.palletsprojects.com",
+  streamlit: "https://streamlit.io",
+  yolo: "https://docs.ultralytics.com/",
+  gemini: "https://aistudio.google.com/"
   // add more
 }
