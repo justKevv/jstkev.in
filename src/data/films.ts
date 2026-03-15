c46f8a2f478bf148229ae6933dcd5c8299c85501
justKevv <kevinbramastaa@gmail.com>
2026-03-15 20:52:05 +0800

    feat(films): add films page and data for top movies

---
+++ src/data/films.ts
@@ -0,0 +1,22 @@
+export const topFilms = [
+  {
+    title: "tick, tick… BOOM!",
+    year: "2021",
+    poster: "https://image.tmdb.org/t/p/w600_and_h900_face/DPmfcuR8fh8ROYXgdjrAjSGA0o.jpg",
+  },
+  {
+    title: "Past Lives",
+    year: "2023",
+    poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/k3waqVXSnvCZWfJYNtdamTgTtTA.jpg",
+  },
+  {
+    title: "Shin Godzilla",
+    year: "2016",
+    poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/jPNShaWZMpVF0iQ7j1dvTuZLD20.jpg",
+  },
+  {
+    title: "Dune: Part Two",
+    year: "2024",
+    poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/1pdfLvkbY9ohJlCjQH2CZjjYVvJ.jpg",
+  },
+];
