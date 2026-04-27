5792f136e3ed73609506066ece6419d9dc003785
justKevv <kevinbramastaa@gmail.com>
2026-04-27 22:15:39 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,10 +10,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: "https://jstkev.in",
-  trailingSlash: "never",
-  build: {
-    format: "file"
-  },
   vite: {
     plugins: [tailwindcss()]
   },
