8e3bbbe629be4438c1e292cf7a0ed62710e6152b
justKevv <kevinbramastaa@gmail.com>
2026-04-28 12:49:30 +0800

    please fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,6 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
-import cloudflare from '@astrojs/cloudflare';
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://jstkev.in",
