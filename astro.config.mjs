a9e31f540705153ee4cf5f45453c93ff21799576
justKevv <kevinbramastaa@gmail.com>
2026-04-28 11:04:37 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,14 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://jstkev.in",
+  output: 'static',
+  adapter: cloudflare,
+  trailingSlash: 'never',
   vite: {
     plugins: [tailwindcss()]
   },
