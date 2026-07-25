d36270d615e059d1df9f388ab1463dc9523acd0d
justKevv <kevinbramastaa@gmail.com>
2026-07-25 18:25:20 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,6 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
-import playformCompress from '@playform/compress';
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://jstkev.in",
@@ -17,5 +15,5 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-  integrations: [mdx(), sitemap(), playformCompress()],
+  integrations: [mdx(), sitemap()],
 });
